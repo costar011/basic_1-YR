ec0c6dcc2fa2292b84c21b184e66e9edd52218a4
YERIM <dnaer456@gmail.com>
2020-10-30 21:02:16 +0900

    server start

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
 import express from "express";
+import morgan from "morgan";
+
+const PORT = 7000;
+
+const app = express();
+app.use(morgan(`dev`));
+
+app.listen(PORT, () => {
+  console.log(`Server Start ${PORT} `);
+});
